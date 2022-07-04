8b026d7ba5824ddbdc657de8cf0a91fc98ee5eda
okubobarros <okubobarros@gmail.com>
2022-07-04 10:02:19 -0300

    controle-tela

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VERADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                name: 'Fulano',
+                partido: 'Palmeiras',
+                fotos: [
+                    {url: '38111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                name: 'Ciclano',
+                partido: 'Palmeiras-Brasil',
+                fotos: [
+                    {url: '77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                name: 'Deltrano',
+                partido: 'ADV',
+                vice: 'Balrog',
+                fotos: [
+                    {url: '6.jpg', legenda: 'Prefeito'},
+                    {url: '9.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                name: 'Zulano',
+                partido: 'PSDV',
+                vice: 'Ken',
+                fotos: [
+                    {url: '10.jpg', legenda: 'Prefeito'},
+                    {url: '3.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
