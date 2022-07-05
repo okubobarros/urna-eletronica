8f525f9990997c173ec546b9a4617358a1e3f5f9
okubobarros <okubobarros@gmail.com>
2022-07-05 17:15:33 -0300

    final-adjust

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = false;
+let votos = [];
 
 function comecarEtapa() {
     let etapa = etapas[etapaAtual];
 
     let numeroHtml = '';
+    numero = '';
 
     for(let i=0; i<etapa.numeros; i++) {
         if(i === 0) {
@@ -30,9 +33,43 @@ function comecarEtapa() {
 } 
 
 function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=> {
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.requestFullscreen.display = 'block';
+        aviso.style.display = 'block';
+        descrição.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+        
+        let fotosHtml = '';
+        for(let i in candidato.fotos) {
+            if(candidato.fotos[i].small) {
 
-}
+            fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
 
+            } else {
+                fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+            }
+        }
+
+    
+        lateral.innerHTML = fotosHtml;
+    }else{
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+
+    }
+
+    console.log('Candidato' , candidato);
+    }
+    
 
 function clicou(n) {
     let elNumero = document.querySelector('.numero.pisca');
@@ -49,16 +86,51 @@ function clicou(n) {
     }
 }
 
-function branco() {
-    alert("Clicou em BRANCO ");
+function branco(){
+
+    numero = '';
+    votoBranco = true;
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    numeros.innerHTML = '';
+    descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+    lateral.innerHTML = '';
+
 }
 
-function corrige() {
-    alert("Clicou em CORRIGE ");
+function corrige(){
+comecarEtapa();
 }
 
-function confirma() {
-    alert("Clicou em CONFIRMA ");
-}
+function confirma(){
 
+    let etapa = etapas[etapaAtual];
+
+    let votoConfirmado = false;
+
+    if(votoBranco === true){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
+    }else if(numero.length === etapa.numeros){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
+    }
+
+    if(votoConfirmado){
+        etapaAtual++;
+    if(etapas[etapaAtual] !== undefined){
+        comecarEtapa();
+    }else{
+        document.querySelector('.tela').innerHTML = '<div class="aviso--gigante grande pisca">FIM</div>';
+        console.log(votos);
+    }
+
+    }
+}
 comecarEtapa();
